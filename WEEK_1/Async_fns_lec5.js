fe15423fdeab1e349e35b7d56f40256400365f87
Paras Waghela <paraswaghela777@gmail.com>
2023-12-18 20:34:14 +0530

    week 2 class 5

--- WEEK_1/Async_fns_lec5.js
+++ WEEK_1/Async_fns_lec5.js
@@ -10,6 +10,7 @@
 // console.log("World");
 
 ///////////////////////////////////
+const { promises } = require("dns");
 const fs=require("fs");
 //filesystem module
 
@@ -22,4 +23,74 @@ let a=0;
 for(let i=0;i<100000000;i++){
     a++;
 }
-console.log("hi there 2");
+console.log("hi there 2");// this will runs after above hi there // asyn fn executed at last
+
+
+
+
+/////////////////////////////////// ASYNC FNS
+console.log("hii");
+
+setTimeout(function(){
+    console.log("fron fn1"); //20sec waits ,flow goes ahead , then get executed,if after 20sec the loop is running thrn it waits till loop stops
+},20000);
+
+setTimeout(function(){
+    console.log("fron fn2"); //10sec waits ,,flow goes ahead ,then get executed ,if after 10sec the loop is running thrn it waits till loop stops
+},10000);
+
+let index=0;
+for(let i=0;i<10;i++){ 
+    index++;
+}
+console.log(index);
+
+////////////////////// UGLY WAY TO CREATE OWN ASYNC FN'S
+
+const fs=require('fs');
+
+//my own async fn
+function ParasFile(cb){
+    fs.readFile("a.txt","utf-8",function(err,data){
+        cb(data);
+    });
+}
+
+//callback fn
+function onDone(data){
+    console.log(data);
+}
+
+ParasFile(onDone);
+
+
+/////////////////////// PROMISES , BETTER WAY TO CREATE OWN ASYN FN
+const fs=require('fs');
+
+//own async fn
+function ParasReadFile(){
+    return new promise(function(resolve){
+        fs.readFile("a.txt","utf-80",function(err,data){
+            resolve(data);
+        });
+    })
+}
+
+//callback fn
+function onDone(data){
+    console.log(data);
+}
+
+ParasReadFile().then(onDone);
+
+//OR
+var x=ParasReadFile();
+x.then(onDone);
+
+
+
+
+
+
+
+
